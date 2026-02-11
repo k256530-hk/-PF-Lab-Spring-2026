38bb5212a1a9919cbeb7815bcdb54959afa1951e
k256530-hk <k256530@nu.edu.pk>
2026-02-11 21:55:06 +0500

    Update Task4.c

--- LAB04/Task4.c
+++ LAB04/Task4.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
 
+int main() {
+    char c1, c2, c3, c4, c5;
+    int password;
+
+    printf("Enter username: ");
+    scanf(" %c%c%c%c%c", &c1, &c2, &c3, &c4, &c5);
+
+    printf("Enter password: ");
+    scanf("%d", &password);
+
+    if (c1 == 'a' && c2 == 'd' && c3 == 'm' && c4 == 'i' && c5 == 'n' && password == 1234) {
+        printf("Login Successful\n");
+    } else {
+        printf("Invalid Credentials\n");
+    }
+
+    return 0;
+}
