3b5f9e665b89f0b5a4784b1c92c8a1fa9ab02d48
k256530-hk <k256530@nu.edu.pk>
2026-02-19 20:47:52 +0500

    Create Task6.c

---
+++ LAB05/Task6.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+int a,b,c;
+printf("Enter three sides of triangle:");
+scanf("%d %d %d",&a, &b, &c);
+if(a+b<=c||a+c<=b||b+c<=a){
+    printf("Not a valid traingle\n");
+}
+else{
+    if(a==b && b==c){
+        printf("Equilateral Triangle\n");
+    }
+    else if(a==b||b==c||a==c){
+        printf("Isoceles Triangle\n");
+    }
+    else{
+        printf("Scalene Triangle\n");
+    }
+}
+
+    return 0;
+}
